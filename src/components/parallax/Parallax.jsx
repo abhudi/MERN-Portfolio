a3a9d892ca7f72c7d9d1c323845df59948d867da
unknown <deshmukhabhishek7020@gmail.com.>
2024-03-25 12:46:41 +0530

    first commit

---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,16 @@
+import "./parallax.scss";
+import { motion} from "framer-motion";
+
+const Parallax = ({type}) => {
+
+  return (
+    <div className="parallax" style={{background:type==="services"?"linear-gradient(180deg,#111132, #0c0c1d)": "linear-gradient(180deg, #111132, #505064)"}}>
+      <motion.h1>{type === "services" ? "What We Do?" : "What We Did"}</motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div className="planets"></motion.div>
+      <motion.div className="stars"></motion.div>
+    </div>
+  )
+}
+
+export default Parallax
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,66 @@
+import "./services.scss"
+import {motion} from "framer-motion"
+
+const variants ={
+  initial:{
+    x:-500,
+    y:100,
+    opacity:0
+  },
+  animate:{
+    x:0,
+    opacity:1,
+    y:0,
+    transition:{
+      duration:1,
+      staggerChildren:0.1,
+    },
+  },
+};
+
+const Services = () => {
+
+  
+  return (
+    <motion.div className="services" variants={variants} initial="initial" whileInView="animate">
+      <motion.div className="textContainer" variants={variants}>
+        <p>I Focus on helping you brand <br /> grow and move forward</p>
+      <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1><motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas</h1>
+        </div>
+        <div className="title">
+          <h1><motion.b whileHover={{color:"orange"}}b>For Your </motion.b>Business</h1>
+          <button>WHAT WE DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+          <h2>Branding</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque possimus culpa deserunt harum, dolores nostrum enim doloribus. Corporis corrupti magnam ipsum sed, illum perferendis expedita.</p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+          <h2>Branding</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque possimus culpa deserunt harum, dolores nostrum enim doloribus. Corporis corrupti magnam ipsum sed, illum perferendis expedita.</p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+          <h2>Branding</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque possimus culpa deserunt harum, dolores nostrum enim doloribus. Corporis corrupti magnam ipsum sed, illum perferendis expedita.</p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
+          <h2>Branding</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque possimus culpa deserunt harum, dolores nostrum enim doloribus. Corporis corrupti magnam ipsum sed, illum perferendis expedita.</p>
+          <button>Go</button>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default Services
